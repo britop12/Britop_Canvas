cd862b59158fd5e62b5603e3ea57056ed3289167
Esteban Brito <estebanbritop@gmail.com>
2017-12-27 22:22:01 -0500

    Agregando la estructura HTML y JS

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+var boton1 = document.getElementById("boton1");
+var texto1 = document.getElementById("texto1");
+
+boton1.addEventListener("click", ejecutar);
+
+function ejecutar()
+{
+  console.log("si sirve");
+}
