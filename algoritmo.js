c3845eecd1a50a69114b33dfceff915353c2e953
Esteban Brito <estebanbritop@gmail.com>
2017-12-27 23:03:06 -0500

    Arreglado el primer error

--- algoritmo.js
+++ algoritmo.js
@@ -8,18 +8,18 @@ function ejecutar()
   var numero = parseInt(texto1.value);
   if (numero % 3 == 0)
   {
-     resultado.innerHTML = numero +  " Es fizz";
+     resultado.innerHTML = numero + ", <strong>Es fizz</strong>";
   }
   if (numero % 4 == 0)
   {
-    resultado.innerHTML = numero + " Es Buzz";
+    resultado.innerHTML = numero + ", <strong>Es Buzz</strong>";
   }
   if (numero % 3 == 0 & numero % 4 == 0)
   {
-    resultado.innerHTML = numero + " Es FizzBuzz";
+    resultado.innerHTML = numero + ", <strong>Es FizzBuzz</strong>";
   }
-  else
+  if (numero % 3 != 0 & numero % 4 != 0)
   {
-    resultado.innerHTML = numero + " No es Fizz, ni  Buzz porque el residuo es diferente a 0"
+    resultado.innerHTML = numero + ", <strong>No es Fizz, ni  Buzz porque el residuo es diferente a 0</strong>"
   }
 }
